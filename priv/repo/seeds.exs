dd0a0ced18cadd1cdb915b19e36e3d13e655cfb4
koga1020 <koga.shozo1020@gmail.com>
2019-12-14 15:09:00 +0900

    update seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,11 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     MixReleaseExample.Repo.insert!(%MixReleaseExample.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias MixReleaseExample.Accounts
+
+1..10
+|> Enum.map(fn itr ->
+  %{
+    name: "test user #{itr}"
+  }
+end)
+|> Enum.each(fn attrs ->
+  Accounts.create_user(attrs)
+end)
