c2e8c5c76024f1d64311ba28d7c455054127a769
koga1020 <koga.shozo1020@gmail.com>
2019-12-14 15:48:29 +0900

    show user records

--- lib/mix_release_example_web/controllers/page_controller.ex
+++ lib/mix_release_example_web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule MixReleaseExampleWeb.PageController do
   use MixReleaseExampleWeb, :controller
+  alias MixReleaseExample.Accounts
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    users = Accounts.list_users()
+    render(conn, "index.html", users: users)
   end
 end
